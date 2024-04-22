81c3930c1b58ba4d8cd54fa00dd4e2e08a0b264d
Flourish <flourishferguson@gmail.com>
2024-04-22 15:01:14 +0100

    Calc

---
+++ src/main/java/com/example/newprogram/MyProgram.java
@@ -0,0 +1,40 @@
+package com.example.newprogram;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import static javafx.application.Application.launch;
+
+
+public class MyProgram {
+        TextField screen;
+        Button btn1,btn2, btn3, btn4, btn5, btn6,
+                btn7,btn8,btn9,btn0,btnPlus,btnMinus,
+                btnDivide,btnMultiply,btnModulus,
+                btnSquare,btnEquals,btnClear,
+                btnBackSpace,btnDot,btnSqrt,btnExponential,btnDisable;
+        double num1, num2;
+        String operation;
+        public static void main(String[] args) {
+            launch();
+        }
+
+
+    }
+}
